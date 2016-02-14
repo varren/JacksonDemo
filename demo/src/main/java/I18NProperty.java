74e68310adf23673223b819bacf5b1e3b4f7aa96
Petr Grabovoy <varren@gmail.com>
2016-02-14 21:10:41 +0300

    new better fix

--- demo/src/main/java/I18NProperty.java
+++ demo/src/main/java/I18NProperty.java
@@ -1,29 +1,30 @@
-package wrapperddemo;
-
 import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
+import java.lang.String;
+import java.util.ArrayList;
 import java.util.Locale;
 
 @JsonRootName(value = "name")
-public class I18NPropertyWrapper<T> {
+@JsonFormat(shape=JsonFormat.Shape.OBJECT)
+public class I18NProperty<T> extends ArrayList<I18NProperty.Content<T>>{
 
-    public I18NPropertyDemo2<T> getNames() {
-        return names;
+    public static class Content<T> {
+        public Locale i18n;
+        public T val;
+        @Override
+        public String toString() {
+            return "Locale: "+ i18n.toString() +" value: "+ String.valueOf(val);
+        }
     }
-
-    public void setNames(I18NPropertyDemo2<T> names) {
-        this.names = names;
-    }
-
-    private I18NPropertyDemo2<T> names = new I18NPropertyDemo2<T>();
 
     @JsonAnySetter
     public void set(String key, T value) {
-        I18NPropertyDemo2.Content<T> c = new I18NPropertyDemo2.Content<T>();
+        Content<T> c = new Content<T>();
         c.i18n = new Locale(key);
         c.val = value;
-        names.add(c);
+        this.add(c);
     }
 
     @Override
@@ -31,7 +32,7 @@ public class I18NPropertyWrapper<T> {
         StringBuilder res = new StringBuilder();
         res.append("[");
         String delim = "";
-        for(I18NPropertyDemo2.Content<T> elem: this.names) {
+        for(I18NProperty.Content elem: this) {
             res.append(delim);
             delim = ", ";
             res.append(String.valueOf(elem));
--- demo/src/main/java/Main.java
+++ demo/src/main/java/Main.java
@@ -1,7 +1,6 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import wrapperddemo.I18NPropertyWrapper;
 
 import java.io.IOException;
 
@@ -9,41 +8,19 @@ public class Main {
     private static final String s = "{\"name\": {\"en\": \"foo\", \"jp\": \"bar\"}}";
     private static final String s2 = "{\"name\": {\"en\": {\"title\":\"foo\", \"id\":1}, \"jp\": {\"title\":\"bar\", \"id\":2}}}";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+        mapper.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
 
-        runMainDemo(mapper);
-        runWrapperTest(mapper);
-    }
-
-
-
-    public static void runMainDemo(ObjectMapper mapper){
         try {
-            I18NPropertyDemo1<String> sweOut1 = mapper.readValue(s, new TypeReference<I18NPropertyDemo1<String>>() {});
-            I18NPropertyDemo1<TestObject> sweOut2 = mapper.readValue(s2, new TypeReference<I18NPropertyDemo1<TestObject>>() {});
+            I18NProperty<String> sweOut1 = mapper.readValue(s, new TypeReference<I18NProperty<String>>() {});
+            I18NProperty<TestObject> sweOut2 = mapper.readValue(s2, new TypeReference<I18NProperty<TestObject>>() {});
 
-            System.out.println("\n--- JSON to JAVA DEMO1 sweOut1---\n" + sweOut1);
-            System.out.println("\n--- JSON to JAVA DEMO1 sweOut2---\n" + sweOut2);
+            System.out.println("\n--- JSON to JAVA DEMO1 sweOut1---\n" + sweOut1 +" "+  sweOut1.size());
+            System.out.println("--- JSON to JAVA DEMO1 sweOut2---\n" + sweOut2+" "+  sweOut2.size());
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static void runWrapperTest(ObjectMapper mapper) {
-        try {
-            I18NPropertyWrapper<String> wrapper1 = mapper.readValue(s, new TypeReference<I18NPropertyWrapper<String>>() {});
-            I18NPropertyWrapper<TestObject> wrapper2 = mapper.readValue(s2, new TypeReference<I18NPropertyWrapper<TestObject>>() {});
-
-            System.out.println("\n--- JSON to JAVA DEMO2 sweOut1---\n" + wrapper1.getNames());
-            System.out.println("\n--- JSON to JAVA DEMO2 sweOut2---\n" + wrapper2.getNames());
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
     }
 }
